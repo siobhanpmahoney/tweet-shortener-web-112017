41bc928b53647efef6972e4d93d81e1fb2963caa
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-11-01 21:22:00 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def dictionary
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet = tweet.split(/\b/)
+  tweet.each_with_index do |word, i|
+    dictionary.each do |v_long, v_short|
+      if word == v_long
+        tweet[i] = v_short
+      end
+    end
+  end
+  tweet = tweet.join
+  tweet 
+end
