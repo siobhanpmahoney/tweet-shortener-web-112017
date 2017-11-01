66e7091a02e74380e4ac71f6d0e71762e745fd23
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-11-01 21:50:16 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,7 +36,7 @@ def selective_tweet_shortener(tweet_array)
     if tweet.length < 130
       puts tweet
     if tweet.length > 140
-      word_substituter(tweet)
+      puts word_substituter(tweet)
     end
   end
 end
