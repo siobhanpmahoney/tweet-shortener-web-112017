44ad3efa935561dbb9731b7ffe7b052944bc5476
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-11-01 22:08:30 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,3 +38,18 @@ def selective_tweet_shortener(tweet)
     tweet
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  shortened_tweet = ""
+  if tweet.length > 140
+    shortened_tweet = word_substituter(tweet)
+    if shortened_tweet > 140 
+      shortened_tweet[0..140] + "..."
+    else 
+      shortened_tweet
+    end
+  else
+    tweet
+  end 
+end 
+
