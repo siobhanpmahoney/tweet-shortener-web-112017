ef691ae760d1c738fd6156d4cb45fea87fc3970b
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-11-01 21:29:37 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -26,7 +26,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweet_array)
-  tweet_array.each do |tweet| 
+  tweet_array.each do |tweet|
     puts word_substituter(tweet)
   end
 end
