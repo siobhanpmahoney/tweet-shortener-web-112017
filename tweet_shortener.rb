f450df91e4904259d2b4057b22f98dce46734fef
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-11-01 21:50:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,7 +32,7 @@ def bulk_tweet_shortener(tweet_array)
 end
 
 def selective_tweet_shortener(tweet_array)
-  tweet_array.each do |tweet|
+  tweet_array.map do |tweet|
     if tweet.length < 130
       puts tweet
     if tweet.length > 140
